2edbb277c332b26045925ede045f22355c13821f
JP Alexander <jpalexa@gmail.com>
2020-06-23 08:37:55 +0100

    Initial "Hello, World" scaffolding C# .NET Core console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
